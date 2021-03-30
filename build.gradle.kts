6b8db05c863f4b66597041c1dc4b0d57fa0965cb
Oleksandr Garmatin <invizible.soft@gmail.com>
2021-03-30 03:17:12 +0300

    Update signing config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,14 @@
  * Distributed under the GNU GPL v2 with additional terms. For full terms see the file doc/LICENSE.txt
  */
 
+val keystorePropertiesFile by extra("/usr/local/KStore/keystore.properties")
+val keystoreProperties = java.util.Properties()
+keystoreProperties.load(java.io.FileInputStream(keystorePropertiesFile))
+
+val keystoreFile by extra("/usr/local/KStore/pan_alexander.jks")
+val keystorePassword by extra(keystoreProperties["storePassword"])
+val keystoreAliasPassword by extra(keystoreProperties["keyBPassword"])
+val keystoreAlias by extra(keystoreProperties["keyBAlias"])
 
 buildscript {
     repositories {
